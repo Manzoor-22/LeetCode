ae5c63b381d0680808cbc10a4d205f4942fe7c96
Manzoor-22 <110250967+Manzoor-22@users.noreply.github.com>
2024-07-14 23:35:09 +0530

    Time: 0 ms (100%), Space: 41.2 MB (69.39%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+        goal += goal;
+
+        return goal.contains(s);
+    }
+}
