b9c4a643b92d466876084e5231e2de1ae70e7841
Manzoor-22 <110250967+Manzoor-22@users.noreply.github.com>
2024-08-01 19:55:58 +0530

    Time: 0 ms (100%), Space: 42 MB (94.58%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -3,8 +3,8 @@ class Solution {
         int count = 0;
 
         for(int i = 0; i < details.length; i++){
-            int tens = Character.getNumericValue(details[i].charAt(11));
-            int ones = Character.getNumericValue(details[i].charAt(12));
+            int tens = details[i].charAt(11) - '0';
+            int ones = details[i].charAt(12) - '0';
             int age = (tens * 10) + ones;
             if(age > 60)
                 count++;
