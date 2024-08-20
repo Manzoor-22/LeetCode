9decb470c606aefef4f1598ae153d611e940450c
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-08-20 21:21:45 +0530

    Time: 7 ms (66.62%), Space: 47.5 MB (89.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,37 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, int[]> hm = new HashMap<>();
-
-        for(int i = 0; i < strs.length; i++){
-            int[] characters = new int[26];
-            for(int j = 0; j < strs[i].length(); j++){
-                characters[strs[i].charAt(j) - 'a']++;
-            }
-            hm.put(strs[i], characters);
-        }
+        HashMap<String, List<String>> hm = new HashMap<>();
         
-        List<List<String>> ans = new ArrayList<>();
-        
-        boolean[] isUsed = new boolean[strs.length];
+        for(String word : strs){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
 
-        for(int i = 0; i < strs.length; i++){
-            ArrayList<String> curr = new ArrayList<>();
-            if(isUsed[i]){
-                continue;
+            if(!hm.containsKey(sortedWord)){
+                hm.put(sortedWord, new ArrayList<>());
             }
-            curr.add(strs[i]);
-            for(int j = i+1; j < strs.length; j++){
-                if(Arrays.equals( hm.get(strs[i]) , hm.get(strs[j]) )){
-                    if(isUsed[j] == false && isUsed[i] == false){
-                       curr.add(strs[j]);
-                       isUsed[j] = true;
-                    }
-                }
-            }
-            isUsed[i] = true;
-            ans.add(curr);
+
+            hm.get(sortedWord).add(word);
         }
 
-        return ans;
+        return new ArrayList<>(hm.values());
     }
 }
