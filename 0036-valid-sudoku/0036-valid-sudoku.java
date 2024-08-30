726f8e7f99c9a89e9b98639fac66bcad9227f47b
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-08-30 19:12:43 +0530

    Time: 21 ms (5.68%), Space: 45 MB (16.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set seen = new HashSet<>();
+        Set contents = new HashSet<>();
 
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] != '.'){
-                    String b = "(" + board[i][j] + ")";
-                    if(!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
+                    String val = "(" + board[i][j] + ")";
+
+                    if(!contents.add(i+val) || !contents.add(val+j) || !contents.add(i/3+val+j/3)){
                         return false;
+                    }
                 }
             }
         }
