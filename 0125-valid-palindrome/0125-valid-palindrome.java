0b60db1a7395cc5e45d5e135c8faafdf49d28192
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-08-30 23:17:54 +0530

    Time: 2 ms (99.18%), Space: 43 MB (70.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,31 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        if(s.length() == 1){
+            return true;
+        }
+
         int i = 0, j = s.length()-1;
 
         while(i <= j){
             if(!Character.isLetterOrDigit(s.charAt(i))){
                 i++;
-            }else if(!Character.isLetterOrDigit(s.charAt(j))){
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(j))){
                 j--;
             }
-            else{
-                if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+            else {
+                char first = Character.toUpperCase(s.charAt(i));
+                char second = Character.toUpperCase(s.charAt(j));
+
+                if(first != second){
                     return false;
+                }
+
                 i++;
                 j--;
             }
         }
+
         return true;
     }
 }
