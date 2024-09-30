b45d4338d34935e5872f153abfae4a8075958332
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-09-30 18:49:33 +0530

    Time: 182 ms (5.84%), Space: 46.4 MB (5.84%) - LeetHub

---
+++ 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.java
@@ -0,0 +1,32 @@
+class CustomStack {
+    List<Integer> ll;
+    final int size;
+
+    public CustomStack(int maxSize) {
+        ll = new LinkedList<>();
+        size = maxSize;
+    }
+    
+    public void push(int x) {
+        if(ll.size() < size)
+        ll.addLast(x);
+    }
+    
+    public int pop() {
+        return ll.isEmpty() ? -1 : ll.removeLast();
+    }
+    
+    public void increment(int k, int val) {
+        for(int i = 0; i < k && i < ll.size(); i++){
+            ll.set(i, ll.get(i) + val);
+        }
+    }
+}
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack obj = new CustomStack(maxSize);
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * obj.increment(k,val);
+ */
