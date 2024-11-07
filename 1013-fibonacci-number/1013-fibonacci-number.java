1da26d6eeaa1959d5b33fb453a683c6a00388269
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-11-07 15:04:05 +0530

    Time: 8 ms (47.19%), Space: 40.3 MB (52.14%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -4,14 +4,6 @@ class Solution {
             return n;
         }
 
-        int[] seq = new int[n+1];
-        seq[0] = 0;
-        seq[1] = 1;
-
-        for(int i = 2; i <= n; i++){
-            seq[i] = seq[i-1] + seq[i-2];
-        }
-
-        return seq[n];
+        return fib(n-1) + fib(n-2);
     }
 }
