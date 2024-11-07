56287770dee70353e3e75738ecbdf53ecd994307
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-11-07 23:08:42 +0530

    Time: 0 ms (100%), Space: 40.3 MB (53.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,23 @@
 class Solution {
     public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+
+        int ans = getDP(n, dp);
+
+        return ans;
+    }
+
+    public int getDP(int n, int[] dp){
         if(n == 1 || n == 2){
             return n;
         }
 
-        int dp[] = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        if(dp[n] != 0){
+            return dp[n];
         }
-
+        
+        dp[n] = getDP(n-1, dp) + getDP(n-2, dp);
+        
         return dp[n];
     }
 }
