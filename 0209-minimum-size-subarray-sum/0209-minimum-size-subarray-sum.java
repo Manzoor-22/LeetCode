9fba410312f81d910f6291333ece1d06c6fda3d7
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-11-22 01:02:40 +0530

    Time: 1 ms (99.95%), Space: 57.7 MB (78.63%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,23 +1,24 @@
-// Algo Used: Sliding Window
-// TC: O N , SC: O 1
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int n= nums.length;
-        int l=0;
-        int r=0;
-        int sum= 0;
-        int ans= Integer.MAX_VALUE;
-        int currLen= 0;
-        while(r < n){ // run till the right pointer reaches end
-            sum+= nums[r];
-            while(sum >= target){ // if found potential ans then reduce the window and keep on updating the window i.e ans
-                currLen= r - l + 1;
-                ans= Math.min(ans, currLen);
-                sum-= nums[l];
-                l++; // shrinkiing window
+        int ans = Integer.MAX_VALUE;
+        int l = 0, r = 0;
+        int n = nums.length;
+        int currSum = 0;
+
+        while(r < n){
+            currSum += nums[r];
+
+            while(currSum >= target){
+                ans = Math.min(ans, r-l+1);
+                currSum -= nums[l];
+                l++;
             }
-            r++;   // expanding window
+
+            r++;
         }
+
+
+
         return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
