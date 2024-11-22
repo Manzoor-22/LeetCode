bca2c031b529ff1238e8dab7fbcb82d6bd4d8181
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-11-22 22:19:19 +0530

    Time: 1 ms (100%), Space: 45.9 MB (71.26%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,31 +2,36 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int[] sortedArr = new int[n1+n2];
 
-        int i = 0, j = 0, k = 0;
+        int merged[] = new int[n1+n2];
+
+        int i = 0, j = 0;
+        int k = 0;
 
         while(i < n1 && j < n2){
             if(nums1[i] < nums2[j]){
-                sortedArr[k++] = nums1[i++];
+                merged[k] = nums1[i++];
+            } else{
+                merged[k] = nums2[j++];
             }
-            else{
-                sortedArr[k++] = nums2[j++];
-            }
+            k++;
         }
 
         while(i < n1){
-            sortedArr[k++] = nums1[i++];
+            merged[k++] = nums1[i++];
         }
 
         while(j < n2){
-            sortedArr[k++] = nums2[j++];
+            merged[k++] = nums2[j++];
         }
 
-        int length = sortedArr.length;
-        if(length % 2 == 0)
-            return (double)(sortedArr[length/2]+sortedArr[length/2-1])/2;
+        if(merged.length % 2 != 0){
+            return merged[merged.length/2];
+        }
 
-        return sortedArr[length/2];
+        double ans = 0;
+        int midIdx = merged.length/2;
+        ans = (double)(merged[midIdx] + merged[midIdx-1]) / 2;
+        return ans;
     }
 }
