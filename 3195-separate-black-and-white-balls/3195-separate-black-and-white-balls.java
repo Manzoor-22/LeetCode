72122e36457246b7a669bc02a8318be18a780a91
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-11-25 00:23:26 +0530

    Time: 7 ms (98.93%), Space: 45.5 MB (41.03%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -1,16 +1,16 @@
 class Solution {
     public long minimumSteps(String s) {
-        long swap = 0;
         long black = 0;
+        long ans = 0;
 
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '0'){
-                swap += black;
+                ans += black;
             } else{
                 black++;
             }
         }
-        
-        return swap;
+
+        return ans;
     }
 }
