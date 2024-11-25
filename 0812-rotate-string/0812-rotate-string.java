12281bb4c9ccfdff40591cc67f951491b37fcd7a
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-11-25 23:23:25 +0530

    Time: 0 ms (100%), Space: 41.6 MB (28.85%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,25 +1,11 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        StringBuilder sb = new StringBuilder(goal);
-
         if(s.length() != goal.length()){
             return false;
         }
+        
+        s += s;
 
-        if(s.equals(goal)){
-            return true;
-        }
-
-        for(int i = 0; i < s.length(); i++){
-            char last = sb.charAt(sb.length()-1);
-            sb.deleteCharAt(sb.length()-1);
-            sb.insert(0, last);
-
-            if(sb.toString().equals(s)){
-                return true;
-            }
-        }
-
-        return false;
+        return s.contains(goal);
     }
 }
