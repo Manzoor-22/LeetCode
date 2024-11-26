93c12d27dfcad094dcf6f3e59c35201788367e62
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-11-27 00:02:00 +0530

    Time: 21 ms (51.42%), Space: 45.4 MB (22.79%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,28 +1,37 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
-
-        int maxLen = 1;
-        int start = 0;
-        int end = 0;
-        boolean[][] dp = new boolean[s.length()][s.length()];
-
-        for (int i = 0; i < s.length(); ++i) {
-            dp[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    if (i - j + 1 > maxLen) {
-                        maxLen = i - j + 1;
-                        start = j;
-                        end = i;
-                    }
+        int maxLen = 0;
+        String ans = "";
+        
+        for(int i = 0; i < s.length(); i++){
+            // odd length
+            int left = i, right = i;
+            
+            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                int currLen = right-left+1;
+                if(currLen > maxLen){
+                    maxLen = currLen;
+                    ans = s.substring(left, right+1);
                 }
+                
+                left--;
+                right++;
+            }
+            
+            // even length
+            left = i;
+            right = i+1;
+            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                int currLen = right-left+1;
+                if(currLen > maxLen){
+                    maxLen = currLen;
+                    ans = s.substring(left, right+1);
+                }
+                left--;
+                right++;
             }
         }
-
-        return s.substring(start, end + 1);
+        
+        return ans;
     }
 }
