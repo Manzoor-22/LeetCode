25df1d279c6df2c07fcedba0b3684488bd9907a0
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2024-12-06 19:30:16 +0530

    Time: 6 ms (98%), Space: 44.8 MB (26.43%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,40 +1,37 @@
 class Solution {
-    static class Info implements Comparable<Info>{
-        String str;
-        int freq;
+    class Info implements Comparable<Info>{
+        String key;
+        int val;
 
-        public Info(int freq, String str){
-            this.freq = freq;
-            this.str = str;
+        public Info(String key, int val){
+            this.key = key;
+            this.val = val;
         }
 
         @Override
-        public int compareTo(Info s1){
-            if(s1.freq == this.freq){
-                return this.str.compareTo(s1.str);
-            }
-            return s1.freq-this.freq;
+        public int compareTo(Info s){
+            if(s.val == this.val)
+                return this.key.compareTo(s.key);
+
+            return s.val - this.val;
         }
     }
 
     public List<String> topKFrequent(String[] words, int k) {
         HashMap<String, Integer> hm = new HashMap<>();
-        for(int i = 0; i < words.length; i++){
-            if(!hm.containsKey(words[i])){
-                hm.put(words[i], 1);
-            } else{
-                hm.put(words[i], hm.get(words[i])+1);
-            }
-        }
-
-        PriorityQueue<Info> pq = new PriorityQueue<>();
-        for(Map.Entry<String, Integer> itr : hm.entrySet()){
-            pq.add(new Info(itr.getValue(), itr.getKey()));
+        for(String s : words){
+            hm.put(s, hm.getOrDefault(s, 0)+1);
         }
 
         List<String> ans = new ArrayList<>();
-        for(int i = 0; i < k; i++){
-            ans.add(pq.poll().str);
+        PriorityQueue<Info> pq = new PriorityQueue<>();
+
+        for(Map.Entry<String, Integer> itr : hm.entrySet()){
+            pq.add(new Info(itr.getKey(), itr.getValue()));
+        }
+
+        for(int i = 1; i <= k; i++){
+            ans.add(pq.poll().key);
         }
 
         return ans;
