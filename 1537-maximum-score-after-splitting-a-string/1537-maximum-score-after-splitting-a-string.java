8cd3fe97ca1f3c74ab6f5075e917aec37b680c3e
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2025-01-02 00:28:07 +0530

    Time: 1 ms (97.36%), Space: 41.5 MB (49.68%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,32 +1,28 @@
 class Solution {
     public int maxScore(String s) {
+        int leftZ = 0;
+        int rightO = 0;
         int n = s.length();
-        int[] leftZ = new int[s.length()];
-        leftZ[0] = s.charAt(0) == '0' ? 1 : 0;
-        for(int i = 1; i < leftZ.length; i++){
-            if(s.charAt(i) == '0'){
-                leftZ[i] = 1 + leftZ[i-1];
-            }
-            else{
-                leftZ[i] = leftZ[i-1];
-            }
-        }
 
-        int[] rightZ = new int[s.length()];
-        rightZ[n-1] = s.charAt(n-1) == '1' ? 1 : 0;
-        for(int i = n-2; i >= 0; i--){
+        leftZ = s.charAt(0) == '0' ? 1 : 0;
+
+        for(int i = n-1; i >= 1; i--){
             if(s.charAt(i) == '1'){
-                rightZ[i] = 1 + rightZ[i+1];
-            }
-            else{
-                rightZ[i] = rightZ[i+1];
+                rightO++;
             }
         }
 
         int ans = 0;
+        ans = Math.max(ans, leftZ+rightO);
 
-        for(int i = 1; i < n; i++){
-            ans = Math.max(ans, leftZ[i-1]+rightZ[i]);
+        for(int i = 1; i < n-1; i++){
+
+            if(s.charAt(i) == '0'){
+                leftZ++;
+            } else{
+                rightO--;
+            }
+            ans = Math.max(ans, leftZ+rightO);
         }
 
         return ans;
