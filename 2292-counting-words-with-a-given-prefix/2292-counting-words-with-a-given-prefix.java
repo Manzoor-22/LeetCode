16a2ae7ffc08d64512b690451ade1ec52aac4999
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2025-01-09 23:17:11 +0530

    Time: 1 ms (30.45%), Space: 42.9 MB (69.98%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
@@ -3,11 +3,21 @@ class Solution {
         int ans = 0;
 
         for(String word : words){
-            if(word.startsWith(pref)){
+            if(startsWith(word, pref)){
                 ans++;
             }
         }
 
         return ans;
+    }
+
+    private boolean startsWith(String s1, String s2){
+        for(int i = 0; i < s2.length(); i++){
+            if(i >= s1.length() || s2.charAt(i) != s1.charAt(i)){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
