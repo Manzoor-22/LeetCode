d7d23447202dc941978a3f55b1e93c4787a43842
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2025-01-14 22:29:25 +0530

    Time: 481 ms (29.98%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,1 +1,1 @@
-select (select distinct salary from employee order by salary desc limit 1 offset 1) as SecondHighestSalary;
+select (select distinct salary from employee order by salary DESC limit 1 offset 1) as SecondHighestSalary;
