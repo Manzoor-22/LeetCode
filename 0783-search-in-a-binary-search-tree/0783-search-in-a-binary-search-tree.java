592cee8c535a0395396a85f4d9be91b5a44ae949
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2025-01-20 02:02:51 +0530

    Time: 0 ms (100%), Space: 45.2 MB (82.39%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -18,14 +18,15 @@ class Solution {
         if(root == null){
             return null;
         }
+
         if(root.val == val){
             return root;
         }
+
         else if(root.val > val){
             return searchBST(root.left, val);
         }
-        else{
-            return searchBST(root.right, val);
-        }
+
+        return searchBST(root.right, val);
     }
 }
