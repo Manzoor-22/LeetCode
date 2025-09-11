d9299f5ce447824c315521c9c29b5d1e2603a655
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2025-09-11 23:58:03 +0530

    Time: 1 ms (42.46%), Space: 43.1 MB (99.36%) - LeetHub

--- 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.java
+++ 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.java
@@ -1,22 +1,13 @@
 class Solution {
     public int minOperations(int[] nums) {
-        if(nums.length == 1){
-            return 0;
-        }
-        
-        for(int i = 0; i < nums.length-1; i++){
-            int curr = nums[i];
-            int next = nums[i+1];
+        int count = 0;
 
-            if(curr != next){
-                break;
-            }
-
-            if(i == nums.length-2){
-                return 0;
+        for(int n : nums){
+            if(n == nums[0]){
+                count++;
             }
         }
 
-        return 1;
+        return count == nums.length ? 0 : 1;
     }
 }
