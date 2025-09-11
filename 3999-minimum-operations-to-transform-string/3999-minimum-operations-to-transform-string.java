19a4eb7649eba96f3483b724537fc361678acc27
Manzoor Mohd <110250967+Manzoor-22@users.noreply.github.com>
2025-09-12 00:34:37 +0530

    Time: 30 ms (37.58%), Space: 51.5 MB (77.47%) - LeetHub

--- 3999-minimum-operations-to-transform-string/3999-minimum-operations-to-transform-string.java
+++ 3999-minimum-operations-to-transform-string/3999-minimum-operations-to-transform-string.java
@@ -1,30 +1,12 @@
 class Solution {
     public int minOperations(String s) {
-        int ans = 0;
+        int slowest = 0;
 
-        // Single Character
-        if(s.length() == 1){
-            if(s.charAt(0) == 'a'){
-                return 0;
-            }
-
-            return 26 - (s.charAt(0)-'a');
+        for(char c : s.toCharArray()){
+            int curr = (26 - (c - 'a')) % 26;
+            slowest = Math.max(curr, slowest);
         }
 
-        char[] charArray = s.toCharArray();
-        Arrays.sort(charArray);
-
-        for(int i = 0; i < charArray.length-1; i++){
-            if(charArray[i] != 'a' && charArray[i] != charArray[i+1]){
-                ans += charArray[i+1]-charArray[i];
-            }
-        }
-
-        // Last char was left in the upper loop, calculating for last char
-        if(charArray[charArray.length-1] != 'a'){
-            ans += 26 - (charArray[charArray.length-1] - 'a');
-        }
-
-        return ans;
+        return slowest;
     }
 }
